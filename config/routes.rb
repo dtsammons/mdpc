81a84fa8ec1bfe622f931b8315baa53a3cbbfea2
David Sammons <dsammons@mgewholesale.com>
2018-03-05 10:47:30 -0500

    added Insurance page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-
   get 'doctor', to: 'static_pages#doctor'
-
   get 'about',  to: 'static_pages#about'
+  get 'insurance', to: 'static_pages#insurance'
 end
